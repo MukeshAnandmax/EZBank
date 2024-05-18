246060905789dd73e98451701b3a7bd9fc1ba1aa
Mukesh Anand <Mukeshanandmax@gmail.com>
2024-05-18 16:28:09 +0530

    refactored typo in dto class name

--- accounts/accounts/src/main/java/com/mukesh/accounts/Dto/AccountsDto.java
+++ accounts/accounts/src/main/java/com/mukesh/accounts/Dto/AccountsDto.java
@@ -1,11 +1,9 @@
 package com.mukesh.accounts.Dto;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
-public class AccuntsDto {
+public class AccountsDto {
 
     private Long accountNumber;
     private  String accountType;
